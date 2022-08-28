cb47549bc2b80be76f521a40d7e0e47297c96222
Laura <lauramoerk@hotmail.com>
2022-08-28 20:22:20 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+const buttonLight = document.querySelector("#light");
+const buttonDark = document.querySelector("#dark");
+
+function toggleTheme(e) {
+  const mode = e.target.dataset.theme;
+  if (mode !== "dark") {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+}
+
+buttonLight.addEventListener("click", toggleTheme);
+buttonDark.addEventListener("click", toggleTheme);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    colors:{
+    colors: {
       pri: "#242325",
       sec: "#47454A",
-      ter:"#ADACAF",
-      acc:"#F07A05",
-      blck:"#000",
-      wht:"#FFFFFF",
+      ter: "#ADACAF",
+      acc: "#F07A05",
+      blck: "#000",
+      wht: "#FFFFFF",
     },
     extend: {
       fontFamily: {
-        aboreto: "'Aboreto', cursive"
-      }
+        aboreto: "'Aboreto', cursive",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
